7f7a28ff2c8bd97e8d86eb9fd282484a88bab29e
unknown <>
2018-04-02 10:22:45 +0800

    可以动态图片课程

--- src/view/Promote.vue
+++ src/view/Promote.vue
@@ -62,49 +62,24 @@
     <!-- 主体部分 -->
     <div class="main">
       <!-- 自动轮播 -->
-      <div class="neck">
+      <div class="neck" >
        <mt-swipe :auto="4000" >
-         <mt-swipe-item>
-           <a href="">
-             <img src="../assets/img/1.jpg" alt="">
+         <mt-swipe-item v-for="item in slide_a">
+          <!--$gretUrl在main.js 那里看路径 -->
+            <a :href="$gretUrl+item.link_url" > 
+             <img :src="$gretUrl+item.pic_path" data-holder-rendered="true">
            </a>
+          
+           
          </mt-swipe-item>
 
-         <mt-swipe-item>
-           <a href="">
-             <img src="../assets/img/2.jpg" alt="">
-           </a>
-         </mt-swipe-item>
-
-         <mt-swipe-item>
-           <a href="">
-             <img src="../assets/img/3.jpg" alt="">
-           </a>
-         </mt-swipe-item>
-
-         <mt-swipe-item>
-           <a href="">
-             <img src="../assets/img/4.jpg" alt="">
-           </a>
-         </mt-swipe-item>
-
-         <mt-swipe-item>
-           <a href="">
-             <img src="../assets/img/5.jpg" alt="">
-           </a>
-         </mt-swipe-item>
-
-         <mt-swipe-item>
-           <a href="">
-             <img src="../assets/img/6.jpg" alt="">
-           </a>
-         </mt-swipe-item>
+         
 
        </mt-swipe>
      </div>    
     </div>
     <!-- body部分 -->
-    <div class="body fff" v-for="item in 4 ">
+    <div class="body fff" v-for="item in 4 " >
           <div class="bbox">
             <p class="tit">
               <span class="icon"></span>
@@ -112,10 +87,10 @@
             </p> 
             <div class="list">
               <div class="sbox one">
-                <h2>Java</h2>
+                <h2>111</h2>
                 <p>Spring cloul微服务实战</p>
                 <div>￥366.00
-                  <span>353人</span>
+                  <span>22人</span>
                 </div>
               </div>
               <div class="sbox two">
@@ -129,75 +104,18 @@
             </div>
             <div class="swiper">
               <mt-swipe :auto="0">
-                <mt-swipe-item class="swipe">
-                  <div class="zhuan">
+                <mt-swipe-item class="swipe" v-for="q in 2 ">
+                  <div class="zhuan" v-for="item in kc_a " @click="go(item.id)">
                     <a href="#">
-                      <img src="../assets/img/7.jpg" alt="">
+                      <img v-if="item.smalltalk_img" :src="$gretUrl+item.smalltalk_img" alt="">
                       <div class="txt">
-                        <p class="name">js入门到实践
-                        开发 js都是你的必备技能</p>
-                        <div>高级·1631人在学</div>
-                        <div>￥455.00</div>
+                        <p class="name">{{item.title}}</p>
+                        <div>有{{item.join_num}}人在学</div>
+                        <div>{{item.summary}}</div>
                       </div>
                     </a> 
                   </div>
-                  <div class="zhuan">
-                    <a href="#">
-                      <img src="../assets/img/7.jpg" alt="">
-                      <div class="txt">
-                        <p class="name">js入门到实践
-                        开发 js都是你的必备技能</p>
-                        <div>高级·1631人在学</div>
-                        <div>￥455.00</div>
-                      </div>
-                    </a> 
-                  </div>
-                  <div class="zhuan">
-                    <a href="#">
-                      <img src="../assets/img/7.jpg" alt="" class="img">
-                      <div class="txt">
-                        <p class="name">js入门到实践
-                        开发 js都是你的必备技能</p>
-                        <div>高级·1631人在学</div>
-                        <div>￥455.00</div>
-                      </div>
-                    </a> 
-                  </div>
-                </mt-swipe-item>
-                <mt-swipe-item class="swipe">
-                  <div class="zhuan">
-                    <a href="#">
-                      <img src="../assets/img/7.jpg" alt="">
-                      <div class="txt">
-                        <p class="name">js入门到实践
-                        开发 js都是你的必备技能</p>
-                        <div>高级·1631人在学</div>
-                        <div>￥455.00</div>
-                      </div>
-                    </a> 
-                  </div>
-                  <div class="zhuan">
-                    <a href="#">
-                      <img src="../assets/img/7.jpg" alt="">
-                      <div class="txt">
-                        <p class="name">js入门到实践
-                        开发 js都是你的必备技能</p>
-                        <div>高级·1631人在学</div>
-                        <div>￥455.00</div>
-                      </div>
-                    </a> 
-                  </div>
-                  <div class="zhuan">
-                    <a href="#">
-                      <img src="../assets/img/7.jpg" alt="" class="img">
-                      <div class="txt">
-                        <p class="name">js入门到实践
-                        开发 js都是你的必备技能</p>
-                        <div>高级·1631人在学</div>
-                        <div>￥455.00</div>
-                      </div>
-                    </a> 
-                  </div>
+                  
                 </mt-swipe-item>
               </mt-swipe>
             </div>
@@ -221,23 +139,44 @@
         tt:true,
         tt_1:false,
         isActive: false,
-        isclose:false
-        
-        
+        isclose:false,
+        slide_a:[],
+        kc_a:[]
     }
   },
   components:{
 
   },
+  created(){
+    this.init()
+  },
   methods:{
+       init(){
+          this.$http.get("/api/Mobilehdp",{params:{page_cate:'promote'}})
+          .then((rtnD)=> {
+
+            this.slide_a = rtnD.data
+            // console.log(rtnD)
+          })
+          this.$http.post("/api/promote",{params:{page_cate:'promote'}})
+          //用post获取是要用params
+          .then((rtnD)=> {
+
+            this.kc_a = rtnD.data.data
+            // console.log(rtnD)
+          })
+       },
        handleChange() {
           this.tt_1=true
           // this.tt=false
           this.isActive=true
-          console.log(this.tt)
+          // console.log(this.tt)
         },
         close(){
           this.isclose=true
+        },
+        go(id){
+            this.$router.push('/stalkteacher/'+id)
         }
     }
       
--- src/view/uhome.vue
+++ src/view/uhome.vue
@@ -1,4 +1,4 @@
-<template>
+t<template>
 
 
   <div class="uhomedd">
