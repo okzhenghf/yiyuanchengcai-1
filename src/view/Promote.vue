9fe949854bf011638599faba7ae65b9a50c680a4
smith <3162547163@qq.com>
2018-04-08 10:45:27 +0800

    提升分类切换

--- src/view/Promote.vue
+++ src/view/Promote.vue
@@ -5,125 +5,116 @@
       :hide-footer="true" :hide-header="true" class="hongbao_box"
       v-bind:class="{ motai: isclose}" @click="close()"
       >
-        
-        <div class="neirong" >
-          <div class="bg"  @click="close()"></div>
-          <div class="close"  @click="close()" ></div>
-          <div class="popup">
-            <div class="po_top"
+
+      <div class="neirong" >
+        <div class="bg"  @click="close()"></div>
+        <div class="close"  @click="close()" ></div>
+        <div class="popup">
+          <div class="po_top"
             v-bind:class="{ hide: isActive}"
              v-show="tt" @click="handleChange()" ></div>
-            <div class="po_bot"></div>
-            <div class="qiang"  @click="handleChange()"></div>
-            <div class="lq">
-              <div class="po_title">未领取</div>
-              <div class="img" v-show="tt_1"><img src="../assets/img/late.png" alt=""></div>
-            </div>
+          <div class="po_bot"></div>
+          <div class="qiang"  @click="handleChange()"></div>
+          <div class="lq">
+            <div class="po_title">未领取</div>
+            <div class="img" v-show="tt_1">
+              <img src="../assets/img/late.png" alt=""></div>
           </div>
-        </div>    
+        </div>
+      </div>
     </b-modal>
 
     <div class="head">
-        <div class="first">
-          <mt-search
+      <div class="first">
+        <mt-search
             :v-model="value"
             cancel-text="取消"
-            placeholder="搜索">
-          </mt-search>
-        </div>
+            placeholder="搜索"></mt-search>
+      </div>
     </div>
 
     <div class="face">
-        <ul >
+      <ul >
+        <li @click="is_cate=false" :class="{'is-active':!is_cate}">首页</li>
+        <li v-for="(item,n) in ke_cheng_cate" >
+          <!-- (item,n) n是下标 -->
 
-          <li v-for="(item,n) in ke_cheng" > 
-            <!-- (item,n) n是下标 -->
+          <div :class="{'is-active':n == cur_kc_cate_index && is_cate}" @click="change_ke_cheng_cate(n,item.id)">{{item.cate_name}}</div>
 
-            <div :class="{'is-active':n == cur_kc_cate_index}" @click="change_ke_cheng_cate(n,item.id)">{{item.cate_name}}</div>
-            
-          </li>
-          
-        </ul>
+        </li>
+
+      </ul>
     </div>
-    
 
-                
- 
-
-             
-
-    <!-- 主体部分 -->
-    <div class="main">
+    <div v-show="!is_cate">
+      <!-- 主体部分 -->
+      <div class="main">
         <!-- 自动轮播 -->
         <div class="neck" >
-         <mt-swipe :auto="4000" >
-           <mt-swipe-item v-for="item in slide_a">
-            <!--$gretUrl在main.js 那里看路径 -->
-              <a :href="$gretUrl+item.link_url" > 
-               <img :src="$gretUrl+item.pic_path" data-holder-rendered="true">
-             </a>
-            
-             
-           </mt-swipe-item>
+          <mt-swipe :auto="4000" >
+            <mt-swipe-item v-for="item in slide_a">
+              <!--$gretUrl在main.js 那里看路径 -->
+              <a :href="$gretUrl+item.link_url" >
+                <img :src="$gretUrl+item.pic_path" data-holder-rendered="true"></a>
 
-           
+            </mt-swipe-item>
 
-         </mt-swipe>
-       </div>    
-    </div>
-    <!-- body部分 -->
-    <div class="body fff" v-for="item in type_id " >
-            <div class="bbox">
-              <p class="tit">
-                <span class="icon"></span>
-                {{item.cate_type}}
-              </p> 
-              <!-- <div class="list">
-                <div class="sbox one">
-                  <h2>111</h2>
-                  <p>Spring cloul微服务实战</p>
-                  <div>￥366.00
-                    <span>22人</span>
-                  </div>
+          </mt-swipe>
+        </div>
+      </div>
+      <!-- body部分 -->
+      <div class="body fff" v-for="item in type_id " >
+        <div class="bbox">
+          <p class="tit">
+            <span class="icon"></span>
+            {{item.cate_type}}
+          </p>
+
+          <div class="swiper">
+
+            <div class="zhuan" v-for="item2 in item.s " @click="go(item.id)">
+
+              <a href="#">
+                <img v-if="item2.smalltalk_img" :src="$gretUrl+item2.smalltalk_img" alt="">
+                <div class="txt">
+                  <p class="name">{{item2.title}}</p>
+                  <p>主讲人：{{item2.real_name}}</p>
+
+                  <div>有{{item2.join_num}}人在学</div>
+                  <div>￥：{{item2.price}}元</div>
                 </div>
-                <div class="sbox two">
-                  <h2>Java</h2>
-                  <p>Spring cloul微服务实战</p>
-                  <div>￥366.00
-                    <span class="span">353人</span>
-                  </div>
-                </div>
-                
-              </div> -->
-              <div class="swiper">
-                <!-- <mt-swipe :auto="0">
-                  <mt-swipe-item class="swipe" v-for="item1 in 2 "> -->
-                    <!-- {{item}} -->
-                    <div class="zhuan" v-for="item2 in item.s " @click="go(item.id)">
-
-                      <a href="#">
-                        <img v-if="item2.smalltalk_img" :src="$gretUrl+item2.smalltalk_img" alt="">
-                        <div class="txt">
-                          <p class="name">{{item2.title}}</p>
-                          <p>主讲人：{{item2.real_name}}</p>
-
-                          <div>有{{item2.join_num}}人在学</div>
-                          <div>￥：{{item2.price}}元</div>
-                        </div>
-                      </a>
-                    </div>
-                    
-                 <!--  </mt-swipe-item>
-                </mt-swipe> -->
-              </div>
+              </a>
             </div>
+
+            <!--  </mt-swipe-item> </mt-swipe>
+          -->
+        </div>
+      </div>
     </div>
-      
-    <div class="zuixia"></div>
+
   </div>
+  <div v-show="is_cate">
+    <div class="swiper">
+      <div class="zhuan" v-for="item2 in change_cate_data " @click="go(item2.id)">
+
+        <a href="#">
+          <img v-if="item2.smalltalk_img" :src="$gretUrl+item2.smalltalk_img" alt="">
+          <div class="txt">
+            <p class="name">{{item2.title}}</p>
+            <p>主讲人：{{item2.real_name}}</p>
+
+            <div>有{{item2.join_num}}人在学</div>
+            <div>￥：{{item2.price}}元</div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="zuixia"></div>
+</div>
 </template>
 
-  <script type="es6">
+<script type="es6">
   import { Search } from 'mint-ui';
   import {mapState,mapMutations} from 'vuex'
   import { Toast,MessageBox,Indicator } from 'mint-ui' 
@@ -141,7 +132,10 @@
         cur_kc_cate_index:0,
         type_id:[],
         ke_cheng:[],
-        cate:[]
+        cate:[],
+        ke_cheng_cate:[],
+        is_cate:false,//默认显示首页
+        change_cate_data:[],//切换的分类数据
     }
   },
   components:{
@@ -158,13 +152,7 @@
             this.slide_a = rtnD.data
             // console.log(rtnD)
           })
-          // this.$http.post("/api/promote",{params:{page_cate:'promote'}})
-          // //用post获取是要用params
-          // .then((rtnD)=> {
-
-          //   this.kc_a = rtnD.data.data
-          //   // console.log(this.kc_a)
-          // })
+         
           this.$http.post("/api/type")
           .then((rtnD)=> {
 
@@ -172,7 +160,7 @@
             // console.log(this.type_id[1].s)
 
             })
-          // new Promise((resolve,reject)=>{
+          
               this.$http.post("/api/Feilei/getFeilei")
               .then((rtnCateD)=> {
 
@@ -180,14 +168,12 @@
               // console.log(this.ke_cheng_cate)
               // resolve(rtnCateD[0]['id'])
             })
-          // }).then((cateId)=>{
+        
             this.$http.post("/api/Feilei")
             .then((rtnD)=> {
 
               this.ke_cheng = rtnD.data.data
-              // console.log(this.ke_cheng)
               })
-          // })
             this.$http.post("/api/Cate")
             .then((rtnD)=> {
 
@@ -211,11 +197,11 @@
         },
         change_ke_cheng_cate(index,cateID){
           this.cur_kc_cate_index = index
+          this.is_cate = true
           console.log(cateID)
-          this.$http.get("/api/Cate",{params:{cateId:cateID}})
+          this.$http.get("/api/Cate/cate_lists",{params:{cateId:cateID}})
             .then((rtnD)=> {
-
-              // this.type_id[this.cur_kc_cate_index ].s = rtnD.data.data
+              this.change_cate_data = rtnD.data.data
               console.log(rtnD)
               })
         },
@@ -224,8 +210,7 @@
   }
   </script>
 
-
-  <style  scoped>
+<style  scoped>
 .modal-content {
     height: 0;
     border: 0;
